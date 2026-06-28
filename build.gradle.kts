e342d463b3653155bcf2669208c0155fb0bf3127
vinay <knvinay21@gmail.com>
2026-06-28 11:29:32 +0100

    Final Polish: Sync Gradle configuration and wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
